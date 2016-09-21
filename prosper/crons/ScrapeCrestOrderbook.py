ef2d2a136d2d86a5797b3ffd461bfbd20b7a3b5b
Lockefox <locke.renard@gmail.com>
2016-09-21 10:26:17 -0700

    merging useful code off FuzzMarket

--- prosper/crons/ScrapeCrestOrderbook.py
+++ prosper/crons/ScrapeCrestOrderbook.py
@@ -5,6 +5,7 @@
 import time #TODO: reduce to TIME or DATETIME?
 import datetime
 import json
+from os import path
 
 import requests
 import pandas
@@ -14,6 +15,33 @@ import plumbum
 from prosper.common.utilities import get_config, create_logger
 from prosper.warehouse.FetchConnection import *
 from prosper.warehouse.Connection import * #OPTIONAL: for Exception handling
+
+HERE = path.abspath(path.dirname(__file__))
+CONFIG_ABSPATH = path.join(HERE, 'cron_config.cfg')
+
+config = get_config(CONFIG_ABSPATH)
+logger = create_logger(
+    'debug-CrestOrderBook',
+    HERE,
+    config,
+    'DEBUG'
+)
+
+def RateLimited(maxPerSecond):
+    '''decorator wrapper for handling ratelimiting'''
+    minInterval = 1.0 / float(maxPerSecond)
+    def decorate(func):
+        lastTimeCalled = [0.0]
+        def rateLimitedFunction(*args,**kargs):
+            elapsed = time.clock() - lastTimeCalled[0]
+            leftToWait = minInterval - elapsed
+            if leftToWait>0:
+                time.sleep(leftToWait)
+            ret = func(*args,**kargs)
+            lastTimeCalled[0] = time.clock()
+            return ret
+        return rateLimitedFunction
+    return decorate
 
 if __name__ == '__main__':
     pass
