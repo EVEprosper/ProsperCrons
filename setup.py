12ffbf3aabed4ed1eb8c92d71363fbc10ee32eef
Lockefox <locke.renard@gmail.com>
2016-09-25 08:08:35 -0700

    updating setup.py to properly wheel project

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 '''wheel setup for Prosper common utilities'''
 
-from os import path
+from os import path, listdir
 from setuptools import setup, find_packages
 
-
 HERE = path.abspath(path.dirname(__file__))
+
+def include_all_subfiles(path_included):
+    '''for data_files {path_included}/*'''
+    local_path = path.join(HERE, path_included)
+    file_list = []
+
+    for file in listdir(local_path):
+        file_list.append(path_included + '/' + file)
+
+    return file_list
+
+def hack_find_packages(include_str):
+    '''setuptools.find_packages({include_str}) does not work.  Adjust pathing'''
+    new_list = [include_str]
+    for element in find_packages(include_str):
+        new_list.append(include_str + '.' + element)
+
+    return new_list
 
 setup(
     name='ProsperWarehouse',
@@ -16,9 +33,22 @@ setup(
     keywords='prosper eveonline api database',
     packages=find_packages(),
     package_data={
-        #TODO
+        ('docs', include_all_subfiles('docs')),
     },
     install_requires=[
-        #TODO
+        'numpy==1.11.1',
+        'pandas==0.18.1',
+        'plumbum==1.6.2',
+        'python-dateutil==2.5.3',
+        'pytz==2016.6.1',
+        'ratelimiter==1.0.2.post0',
+        'requests==2.11.1',
+        'retrying==1.3.3',
+        'six==1.10.0',
+        'ujson==1.35',
+    ],
+    dependency_links=[
+        'https://github.com/EVEprosper/ProsperCommon.git#egg=ProsperCommon',
+        'https://github.com/EVEprosper/ProsperWarehouse.git#egg=ProsperWarehouse' #not quite right
     ]
 )
